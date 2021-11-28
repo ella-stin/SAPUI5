5933449943e3ff863de199395dd8833c3af2a029
Nahye Park <ellapark@naver.com>
2021-11-28 14:00:00 +0000

    Text, Input, Button

---
+++ zui51302/webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("iitp.zui51302.controller.View1", {
+			onInit: function () {
+
+			},
+            onClick: function () {
+                alert("Hello SAPUI5");
+            }
+		});
+	});
---
+++ zui51302/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,11 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require(["iitp/zui51302/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
